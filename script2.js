6b5734a55995f0cdade6ffefc1c22af39be2b6dc
Isabellagg19 <isabellaguioc123@gmail.com>
2026-05-19 02:53:52 -0500

    I fixed the transition effect

---
+++ script2.js
@@ -0,0 +1,20 @@
+function typewriter(elementId, text, speed = 100) {
+    let i = 0;
+    const el= document.getElementById(elementId);
+     if (!el) return;
+
+    function typing() {
+        if (i < text.length) {
+            el.textContent += text.charAt(i);
+            i++;
+            setTimeout(typing, speed);
+        }
+    }
+    typing();
+}
+
+typewriter("message1", "Deep Study", 100);
+typewriter("message2", "Disney Nostalgia", 70);
+typewriter("message3", "Latina Party", 100);
+typewriter("message4", "Bring Back The Old Bangers", 60);
+typewriter("message5", "Sunday Chill", 100);
