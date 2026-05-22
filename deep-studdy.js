7cdfc770a0b18a3196bf2d34ad865b26b21ff376
Isabellagg19 <isabellaguioc123@gmail.com>
2026-05-22 17:28:26 -0500

    funfacts container

--- deep-studdy.js
+++ deep-studdy.js
@@ -1,9 +1,11 @@
 const funfacts= [
     "The Sun is actually white, the Earth's atmosphere scatters blue light, making the sun look yellow to us. In space, it shines in pure white",
-    "Giraffes are lightning magnets, due to their height, giraffes are 30 times more likely to get hit by lightning than humans"
+    "Giraffes are lightning magnets, due to their height, giraffes are 30 times more likely to get hit by lightning than humans",
+    "An average storm cloud weighs around one million tons. That's the equivalent of about 100 elephants floating overhead!",
+    "On Venus, a day takes 243 Earth days to complete, while its orbit around the Sun lasts only 225 Earth days."
 ];
 
-const funfactbtn = document.getElementById("funfact-btn");
+const funfactbtn = document.getElementById("funfact-btn1");
 const text= document.getElementById("facts1");
 
 funfactbtn.addEventListener("click", () => {
