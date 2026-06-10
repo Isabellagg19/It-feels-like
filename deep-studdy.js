30102ac6a945d7d1adf03ce622e8bbbf9a2b034a
Isabellagg19 <isabellaguioc123@gmail.com>
2026-06-10 02:12:18 -0500

    new song suggestion

--- deep-studdy.js
+++ deep-studdy.js
@@ -4,7 +4,7 @@ const funfacts= [
     "An average storm cloud weighs around one million tons. That's the equivalent of about 100 elephants floating overhead!",
     "On Venus, a day takes 243 Earth days to complete, while its orbit around the Sun lasts only 225 Earth days.",
     "The earth rotation is slowing down. This means that, on average, the length of a day increases by around 1.8 seconds per century.",
-    "While they are rich in nerve endings and may play a role in social bonding,mscientists argue that earlobes don’t have any true biological purpose. ",
+    "While they are rich in nerve endings and may play a role in social bonding, scientists argue that earlobes don’t have any true biological purpose. ",
     "The world’s oldest cat lived to 38 years and three days old, it's name was Creme Puff",
     "Polar Bears are not actually white, I mean their fur is white but underneath it, their skin they have a jet-black skin",
     "If you don't like the sound of your own voice it could be because of the bones in your head because they make our voice sound deeper."
